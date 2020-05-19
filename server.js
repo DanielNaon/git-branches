3f4a418f8973b05431d2903aad20c78aa0779145
daniel naon <danielnaon@gmail.com>
2020-05-19 15:34:15 +0300

    change

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path')
 
+app.use(express.static(path.join(__dirname, 'dist')))
 app.listen(port, function(){
     console.log(`listening on port ${port}`)
 })
